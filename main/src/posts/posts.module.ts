c6d5e1971994a2ef3f322e764e9163c9aa936a6e
massfice <adrian.marian.tomasz.larysz@gmail.com>
2024-12-16 18:35:44 +0100

    Providing post service as post service interface

--- main/src/posts/posts.module.ts
+++ main/src/posts/posts.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
 import { PostsController } from './posts.controller';
+import {
+    PostServiceInterface,
+    PostsService,
+} from './posts.service';
 
 @Module({
-  controllers: [PostsController]
+    controllers: [PostsController],
+    providers: [
+        {
+            provide: PostServiceInterface,
+            useClass: PostsService,
+        },
+    ],
 })
 export class PostsModule {}
