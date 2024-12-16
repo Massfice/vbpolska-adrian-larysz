ba46e64051bfe12c67d659837f00e475a902e06b
massfice <adrian.marian.tomasz.larysz@gmail.com>
2024-12-16 18:35:25 +0100

    creating post service and corresponding interface

---
+++ main/src/posts/posts.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { PostModel } from 'models/response/Post.model';
+import { PostDto } from 'models/dto/Post.dto';
+
+export interface PostServiceInterface {
+    fetchAllPosts(): Observable<PostModel[]>;
+    getPostById(id: string): Observable<PostModel>;
+    createPost(data: PostDto): Observable<PostModel>;
+    updatePost(
+        id: string,
+        data: PostDto,
+    ): Observable<PostModel>;
+    deletePost(id: string): Observable<void>;
+}
+
+export const PostServiceInterface = Symbol(
+    'PostServiceInterface',
+);
+
+@Injectable()
+export class PostsService implements PostServiceInterface {
+    fetchAllPosts(): Observable<PostModel[]> {
+        throw new Error('Method not implemented.');
+    }
+    getPostById(id: string): Observable<PostModel> {
+        throw new Error('Method not implemented.');
+    }
+    createPost(data: PostDto): Observable<PostModel> {
+        throw new Error('Method not implemented.');
+    }
+    updatePost(
+        id: string,
+        data: PostDto,
+    ): Observable<PostModel> {
+        throw new Error('Method not implemented.');
+    }
+    deletePost(id: string): Observable<void> {
+        throw new Error('Method not implemented.');
+    }
+}
