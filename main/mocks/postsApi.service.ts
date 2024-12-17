21131a5f382a3512a6d346b5b2f259d7e799d273
massfice <adrian.marian.tomasz.larysz@gmail.com>
2024-12-17 23:25:39 +0100

    updating postsApi service mock

--- main/mocks/postsApi.service.ts
+++ main/mocks/postsApi.service.ts
@@ -5,6 +5,16 @@ import {
     PostState,
 } from '../src/models/response/Post.model';
 
+const post: PostModel = {
+    id: '123',
+    title: 'Title',
+    content: 'Content',
+    state: PostState.DRAFT,
+    hash: 'hash',
+    created_at: new Date(),
+    updated_at: new Date(),
+};
+
 export class PostsApiService
     implements PostsApiServiceInterface
 {
@@ -17,31 +27,47 @@ export class PostsApiService
     fetchAllPosts(
         pattern: string,
     ): Observable<PostModel[]> {
-        throw new Error('Method not implemented.');
+        this._data.push({ pattern, data: {} });
+
+        return of([post]);
     }
 
     getPostById(
         pattern: string,
         id: string,
     ): Observable<PostModel | null> {
-        throw new Error('Method not implemented.');
+        this._data.push({ pattern, data: { id } });
+
+        if (id === '1') {
+            return of(null);
+        }
+
+        return of(post);
     }
 
     createPost(
         pattern: string,
         data: PostModel,
     ): Observable<PostModel> {
-        throw new Error('Method not implemented.');
+        this._data.push({ pattern, data });
+
+        return of(data);
     }
 
     updatePost(
         pattern: string,
         data: Omit<PostModel, 'created_at'>,
     ): Observable<PostModel | null> {
-        throw new Error('Method not implemented.');
+        this._data.push({ pattern, data });
+
+        if (data.id === 'abcd-123') {
+            return of(null);
+        }
+
+        return of({ ...data, created_at: new Date() });
     }
 
     deletePost(pattern: string, id: string): void {
-        throw new Error('Method not implemented.');
+        this._data.push({ pattern, data: { id } });
     }
 }
