d7e9223ce1f713d79f1445c44556a78a353ff90b
massfice <adrian.marian.tomasz.larysz@gmail.com>
2024-12-18 00:30:09 +0100

    adding nats server to configuration

--- main/src/config/index.ts
+++ main/src/config/index.ts
@@ -2,10 +2,18 @@ export interface Config {
     app: {
         port: number;
     };
+    nats: {
+        server: string;
+    };
 }
 
 export const configuration = (): Config => ({
     app: {
         port: parseInt(process.env.PORT || '3000'),
+    },
+    nats: {
+        server:
+            process.env.NATS_SERVER ||
+            'nats://localhost:4222',
     },
 });
