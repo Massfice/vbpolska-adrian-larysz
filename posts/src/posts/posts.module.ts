36dea7ee000f565094844057712ed5906af6e857
massfice <adrian.marian.tomasz.larysz@gmail.com>
2024-12-19 00:31:18 +0100

    initializing mongo db

--- posts/src/posts/posts.module.ts
+++ posts/src/posts/posts.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { PostsController } from './posts.controller';
 import { PostsRepositoryService } from './postsRepository.service';
 import { PostsRepositoryServiceInterface } from '../interfaces/postsRepositoryService.interface';
+import { Post, PostSchema } from './models/db/Post.model';
 
 @Module({
     controllers: [PostsController],
@@ -10,6 +12,11 @@ import { PostsRepositoryServiceInterface } from '../interfaces/postsRepositorySe
             provide: PostsRepositoryServiceInterface,
             useClass: PostsRepositoryService,
         },
+    ],
+    imports: [
+        MongooseModule.forFeature([
+            { name: Post.name, schema: PostSchema },
+        ]),
     ],
 })
 export class PostsModule {}
