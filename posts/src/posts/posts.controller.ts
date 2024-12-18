f58a205ef3700795732de97f2b6d998e19f7825c
massfice <adrian.marian.tomasz.larysz@gmail.com>
2024-12-18 22:51:04 +0100

    injecting posts repository service interface to controller

--- posts/src/posts/posts.controller.ts
+++ posts/src/posts/posts.controller.ts
@@ -1,12 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Inject } from '@nestjs/common';
 import { MessagePattern } from '@nestjs/microservices';
+import { PostDbModel } from './models/db/Post.model';
+import { PostsRepositoryServiceInterface } from '../interfaces/postsRepositoryService.interface';
 
 @Controller()
 export class PostsController {
+    constructor(
+        @Inject(PostsRepositoryServiceInterface)
+        private readonly postsRepositoryService: PostsRepositoryServiceInterface,
+    ) {}
+
     @MessagePattern('posts.fetch')
     something(data: any) {
-        console.log(data);
-
         return [];
     }
 }
