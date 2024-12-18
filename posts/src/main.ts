162b6bbade7a574709d0ed7f4f2b2a0bf6c02ffb
massfice <adrian.marian.tomasz.larysz@gmail.com>
2024-12-18 18:56:40 +0100

    connecting microservices to nats

--- posts/src/main.ts
+++ posts/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+import {
+    MicroserviceOptions,
+    Transport,
+} from '@nestjs/microservices';
 import { AppModule } from './app.module';
+import { Config } from './config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+    const app = await NestFactory.create(AppModule);
+
+    const { server } = app
+        .get<ConfigService<Config>>(ConfigService)
+        .get('nats', { infer: true })!;
+
+    app.connectMicroservice<MicroserviceOptions>({
+        transport: Transport.NATS,
+        options: { servers: [server] },
+    });
+
+    app.startAllMicroservices();
 }
 bootstrap();
