e737bbe919df19a657ce4af1febe7284f2caf61c
massfice <adrian.marian.tomasz.larysz@gmail.com>
2024-12-18 23:56:38 +0100

    updating mock repository service

--- posts/mocks/postsRepository.service.ts
+++ posts/mocks/postsRepository.service.ts
@@ -9,29 +9,92 @@ import { PostModel } from '../src/posts/models/response/Post.model';
 export class PostsRepositoryService
     implements PostsRepositoryServiceInterface
 {
+    private _calls: { data: any }[] = [];
+
+    private _posts: PostModel[] = [];
+
+    set posts(posts: PostModel[]) {
+        this._posts = posts;
+    }
+
+    get calls(): { data: any }[] {
+        return this._calls;
+    }
+
     async fetchPosts(): Promise<PostModel[]> {
-        throw new Error('Method not implemented.');
+        this._calls.push({
+            data: { name: 'fetchAllPosts' },
+        });
+
+        return this._posts;
     }
 
     async getPostById(
         data: IdPostDto,
     ): Promise<PostModel | null> {
-        throw new Error('Method not implemented.');
+        this._calls.push({
+            data: { name: 'getPostById', data },
+        });
+
+        const post = this._posts.find(
+            ({ id: postId }) => data.id === postId,
+        );
+
+        if (!post) {
+            return null;
+        }
+
+        return post;
     }
 
     async createPost(
         data: CreatePostDto,
     ): Promise<PostModel> {
-        throw new Error('Method not implemented.');
+        this._calls.push({
+            data: { name: 'createPost', data },
+        });
+
+        const post: PostModel = {
+            id: data.id,
+            title: data.title,
+            content: data.content,
+            state: data.state,
+            hash: data.hash,
+            created_at: data.created_at,
+            updated_at: data.updated_at,
+        };
+
+        return post;
     }
 
     async updatePost(
         data: UpdatePostDto,
     ): Promise<PostModel | null> {
-        throw new Error('Method not implemented.');
+        this._calls.push({
+            data: { name: 'updatePost', data },
+        });
+
+        const post = this._posts.find(
+            ({ id: postId }) => data.id === postId,
+        );
+
+        if (!post) {
+            return null;
+        }
+
+        post.id = data.id;
+        post.title = data.title;
+        post.content = data.content;
+        post.state = data.state;
+        post.hash = data.hash;
+        post.updated_at = data.updated_at;
+
+        return post;
     }
 
     async deletePost(data: IdPostDto): Promise<void> {
-        throw new Error('Method not implemented.');
+        this._calls.push({
+            data: { name: 'deletePost', data },
+        });
     }
 }
